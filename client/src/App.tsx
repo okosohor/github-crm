7b8f0073bcbc1390dc0d4ec074ed2dede5efbb35
okosohor <kosogor2007@gmail.com>
2025-05-16 11:04:06 +0300

    add title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
+  useEffect(() => {
+    document.title = "GitHub CRM";
+  }, []);
   return (
     <Router>
       <Routes>
